0bedb1f756513b7c22b4e4bb1f0c8c1ab081662a
maksimovdmitrii <maksimov@fhi-berlin.mpg.de>
2020-07-06 19:16:37 +0200

    Center of mass and dihedral angles implementation

--- genseek.py
+++ genseek.py
@@ -1,17 +1,21 @@
 from ase.io import read, write
 from optparse import OptionParser
 
-from ase import neighborlist
-from ase.build import molecule
-from scipy import sparse
+from genseek.modules import *
+import numpy as np
+import sys
 
-from genseek.modules import detect_rotatble
+from random import randint
 
 parser = OptionParser()
 parser.add_option("-g", "--geometry", dest="geometry",
                   help="Geometry")
-parser.add_option("-f", "--format", dest="format",
+parser.add_option("-f", "--geometryformat", dest="format",
                   help="Format of the geometry")
+parser.add_option("--fixedgeometry", dest="fixedgeometry",
+                  help="Format of the fixed geometry")
+parser.add_option("--fixedgeometryformat", dest="fixedgeometryformat",
+                  help="Format of the fixed geometry")
 (options, args) = parser.parse_args()
 
 
@@ -23,15 +27,45 @@ print(atoms)
 
 # Preface
 ## Identify the connectivity of the geometry
-cutOff = neighborlist.natural_cutoffs(atoms)
-neighborList = neighborlist.NeighborList(cutOff, self_interaction=False, bothways=False)
-neighborList.update(atoms)
-matrix = neighborList.get_connectivity_matrix()
-list_of_torsions = detect_rotatble(matrix)
-
+list_of_torsions = detect_rotatble(atoms)
 print(list_of_torsions)
-## Identify the obstacles (Fixed frame)
+
 ## Specify number of molecules 
+molecules = [atoms.copy() for i in range(5)]
+print(molecules)
+
+for i in range(len(molecules)):
+	# Set center of mass
+	value = np.array([randint(-10,10),
+					  randint(-10,10),
+					  randint(-10,10)])
+	set_centre_of_mass(molecules[i], value)
+	for torsion in list_of_torsions:
+		value = randint(0, 360)
+		fixed_indices = carried_atoms(molecules[i], torsion)
+		molecules[i].set_dihedral(angle=value,
+								  a1=torsion[0],
+								  a2=torsion[1],
+								  a3=torsion[2],
+								  a4=torsion[3],
+								  indices=fixed_indices)
+
+# Write the enesemble into file 
+# add the fixed frame also
+# Extend method can be used instead this:
+ensemble = atoms.copy()
+del ensemble[[atom.index for atom in ensemble]]
+for molecule in molecules:
+	ensemble+=molecule
+
+## Identify the obstacles (Fixed frame)
+if options.fixedgeometry is not None:
+	fixed = read(options.fixedgeometry, format=options.fixedgeometryformat)
+	ensemble+=fixed
+
+write("initial.xyz", ensemble, format="xyz")
+
+
 ## Identify the periodic boundary conditions (PBC)
 
 # Start generating of the ensembles
--- genseek/modules.py
+++ genseek/modules.py
@@ -1,4 +1,7 @@
-#    Copyright 2020 Dmitrii Maksimov
+from ase import neighborlist
+from ase.build import molecule
+from scipy import sparse
+import numpy as np
 
 def construct_graph(connectivity_matrix):
     """Constructu graph from connectivity matrix"""
@@ -50,22 +53,85 @@ def create_torsion_list(bond, graph):
     bond[1],
     [i for i in graph[bond[1]] if i!=bond[0]][0])
 
-def detect_rotatble(connectivity_matrix):
+
+def set_centre_of_mass(atoms, new_com):
+    
+    old_positions = atoms.get_positions()
+    old_com = atoms.get_center_of_mass() 
+    atoms.set_positions(old_positions - old_com + new_com)
+
+def create_connectivity_matrix(atoms):
+    cutOff = neighborlist.natural_cutoffs(atoms)
+    neighborList = neighborlist.NeighborList(cutOff, self_interaction=False, bothways=False)
+    neighborList.update(atoms)
+    connectivity_matrix = neighborList.get_connectivity_matrix()
+    return connectivity_matrix
+
+def detect_rotatble(atoms):
     """Detection of all rotatable bonds
-    1. The bonds does not contain terminate atoms
+    2. The bonds does not contain terminate atoms
     2. 
     3. 
     """
+    connectivity_matrix = create_connectivity_matrix(atoms)
     graph = construct_graph(connectivity_matrix)
     indx_not_terminal = [i for i in graph if len(graph[i]) > 1]
     conn = [i for i in connectivity_matrix.keys() 
-            if all(k in indx_not_terminal for k in i)]
-    
+            if all(k in indx_not_terminal for k in i)]   
     list_of_torsions = []
     # If no cycles in the molecule
     if not cycle_exists(graph):
         for bond in conn:
             # Check for the index order
             list_of_torsions.append(create_torsion_list(bond, graph))
-
     return list_of_torsions
+
+
+
+def getroots(aNeigh):
+    #    source: https://stackoverflow.com/questions/10301000/python-connected-components
+    def findroot(aNode,aRoot):
+        while aNode != aRoot[aNode][0]:
+            aNode = aRoot[aNode][0]
+        return aNode, aRoot[aNode][1]
+    myRoot = {}
+    for myNode in aNeigh.keys():
+        myRoot[myNode] = (myNode,0)
+    for myI in aNeigh:
+        for myJ in aNeigh[myI]:
+            (myRoot_myI, myDepthMyI) = findroot(myI, myRoot)
+            (myRoot_myJ, myDepthMyJ) = findroot(myJ, myRoot)
+            if myRoot_myI != myRoot_myJ:
+                myMin = myRoot_myI
+                myMax = myRoot_myJ
+                if myDepthMyI > myDepthMyJ:
+                    myMin = myRoot_myJ
+                    myMax = myRoot_myI
+                myRoot[myMax] = (myMax,max(myRoot[myMin][1]+1,myRoot[myMax][1]))
+                myRoot[myMin] = (myRoot[myMax][0],-1)
+    mytoret = {}
+    for myI in aNeigh:
+        if myRoot[myI][0] == myI:
+            mytoret[myI] = []
+    for myI in aNeigh:
+        mytoret[findroot(myI,myRoot)[0]].append(myI)
+    return mytoret
+
+def insertbreak(graph, atom1, atom2):
+    graph[atom1].pop(graph[atom1].index(atom2))
+    graph[atom2].pop(graph[atom2].index(atom1))
+    return graph
+
+
+def carried_atoms(atoms, positions):
+    """ Returns list of carried atoms """
+    connectivity_matrix = create_connectivity_matrix(atoms)
+    graph = construct_graph(connectivity_matrix)
+    graph_with_break = insertbreak(graph, positions[1], positions[2])
+    if positions[2] in list(getroots(graph_with_break).values())[0]:
+        return list(getroots(graph_with_break).values())[0]
+    else:
+        return list(getroots(graph_with_break).values())[1]
+
+
+
